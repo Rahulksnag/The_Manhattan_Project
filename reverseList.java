38a91ca265fd0c6585c45fe6769b09c2320e593c
Rahul K S <143540192+Rahulksnag@users.noreply.github.com>
2024-11-26 16:21:49 +0530

    Create reverseList.java

---
+++ reverseList.java
@@ -0,0 +1,27 @@
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution {
+    // Function to reverse a linked list.
+    Node reverseList(Node head) {
+        // code here
+        //System.out.println(head.data);
+        Node rev2=null,rev1=null,temp=head;
+        while(temp!=null){
+            rev2=temp.next;
+            temp.next=rev1;
+            rev1=temp;
+            temp=rev2;
+        }
+        
+        return temp;
+    }
+    
+}
